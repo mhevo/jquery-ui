81357af59037e96dd73646a060b77b40213850e2
mhevo <github@mhevo.de>
2021-07-27 20:48:25 +0200

    make them usable for requirejs

--- ui/i18n/datepicker-ar-DZ.js
+++ ui/i18n/datepicker-ar-DZ.js
@@ -6,15 +6,7 @@
 ( function( factory ) {
 	"use strict";
 
-	if ( typeof define === "function" && define.amd ) {
-
-		// AMD. Register as an anonymous module.
-		define( [ "../widgets/datepicker" ], factory );
-	} else {
-
-		// Browser globals
-		factory( jQuery.datepicker );
-	}
+	factory( jQuery.datepicker );
 } )( function( datepicker ) {
 "use strict";
 
